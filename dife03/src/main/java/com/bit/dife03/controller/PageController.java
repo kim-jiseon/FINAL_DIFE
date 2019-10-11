19c7b30bf788bf7232e65cacb4309f62bccc368f
kim-jiseon <52688361+kim-jiseon@users.noreply.github.com>
2019-10-11 17:38:51 +0900

    amdin수정

--- dife03/src/main/java/com/bit/dife03/controller/PageController.java
+++ dife03/src/main/java/com/bit/dife03/controller/PageController.java
@@ -1,5 +1,16 @@
 package com.bit.dife03.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class PageController {
-
+	@RequestMapping("/main")
+	public void main() {
+		
+	}
+	
+	@RequestMapping("/admin")
+	public void admin() {
+		
+	}
 }
