2fe9d9847eb0a89eb47239b1b7c28c2ba0412fe6
choi <choi@DESKTOP-SH232HL>
2019-10-15 14:33:49 +0900

    dd

---
+++ dife03/src/main/java/com/bit/dife03/controller/OrdersController.java
@@ -0,0 +1,18 @@
+package com.bit.dife03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class OrdersController {
+	
+	
+	//Basket 리스트 출력
+//	@ResponseBody
+//	@RequestMapping
+//	public String listBasket()
+//	{
+//		
+//	}
+}
