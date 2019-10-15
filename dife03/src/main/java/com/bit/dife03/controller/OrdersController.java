0a8c9728e790f0c7ddaf730a28083ee679fb7c9f
choi <choi@DESKTOP-SH232HL>
2019-10-15 17:18:15 +0900

    create,insert.text

--- dife03/src/main/java/com/bit/dife03/controller/OrdersController.java
+++ dife03/src/main/java/com/bit/dife03/controller/OrdersController.java
@@ -4,15 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class OrdersController {
 	
 	
+	
 	//Basket 리스트 출력
-//	@ResponseBody
-//	@RequestMapping
-//	public String listBasket()
-//	{
-//		
-//	}
+	@ResponseBody
+	@RequestMapping("/bas_list")
+	public String listBasket()
+	{
+		String str="";
+		ObjectMapper
+		return str;
+	}
 }
