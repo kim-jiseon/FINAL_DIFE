cb5d3be5a65bf7f65bbf760ebecf1e58338516b2
choiminwoo2 <wjrdjs005@naver.com>
2019-11-14 14:49:47 +0900

    pagecontroller error update

--- dife03/src/main/java/com/bit/dife03/controller/PageController.java
+++ dife03/src/main/java/com/bit/dife03/controller/PageController.java
@@ -2,7 +2,9 @@
 package com.bit.dife03.controller;
 
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 public class PageController {
 	@RequestMapping("/main")
@@ -86,109 +88,4 @@ public class PageController {
 	public void cacaotest() {
 		
 	}
-
-package com.bit.dife03.controller;
-
-import javax.servlet.http.HttpSession;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-@Controller
-public class PageController {
-
-	
-	@RequestMapping("/support")
-	public void support() {
-		
-	}
-	
-	@RequestMapping("/board_detail")
-	public void board_detail() {
-		
-	}
-	
-	@RequestMapping("/board_insert")
-	public void board_insert() {
-		
-	}
-	
-	@RequestMapping("/mypage_orders")
-	public void mypage_orders() {
-		
-	}
-	
-	@RequestMapping("/mypage_emoney")
-	public void mypage_emoney() {
-		
-	}
-	
-	@RequestMapping("/admin")
-	public void admin() {
-		
-	}
-	
-	@RequestMapping("/admin_update")
-	public void admin_update() {
-		
-	}
-	
-//	@RequestMapping("/drone")
-//	public void drone() {
-//		
-//	}
-
-	@RequestMapping("/basket")
-	public void basket() {
-		
-	}
-	
-	@RequestMapping("/orders")
-	public void orders() {
-		
-	}
-	
-	@RequestMapping("/ordersDetail")
-	public void ordersDetail() {
-		
-	}
-	
-	@RequestMapping("/payment")
-	public void payment() {
-		
-	}
-	
-	@RequestMapping("/payKG")
-	public void paykg() {
-		
-	}
-	@RequestMapping("/signIn2")
-	public void signIn2() {
-		
-	}
-	
-//	@RequestMapping("/droneDetail")
-//	public void droneDetail() {
-//		
-//	}
-	
-	@RequestMapping("/testtest02")
-	public void test() {
-		
-	}
-
-
-
-//	
-//	@RequestMapping("/signIn")
-//	public void signIn() {
-//		
-//	}
-//	
-//	@RequestMapping("/signUp")
-//	public void signUp() {
-//		
-//	}
-
-
 }
